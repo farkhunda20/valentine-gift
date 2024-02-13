e1cd1c26aa1942ee38d3c086ec995afa3dae0adb
farkhunda20 <farkhundaadel123@gmail.com>
2024-02-13 23:11:58 +0300

    text added

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ yes.addEventListener("click", () => {
 No.addEventListener("click", () => {
   const image = document.createElement("img");
   image.src = "https://cdn.euroflorist.com/Products/600x700/DE21VALMM04S_1.jpg";
+  response.innerHTML =
+    "no worries, I will be your valentine, accept these flowers";
   container.appendChild(response);
   container.appendChild(image);
 });
