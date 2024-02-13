af6ad209456ee87561580033756bf539b14557f1
farkhunda20 <farkhundaadel123@gmail.com>
2024-02-13 23:08:45 +0300

    div deleted

--- script.js
+++ script.js
@@ -7,17 +7,10 @@ yes.addEventListener("click", () => {
   container.appendChild(response);
   response.textContent = "Best of luck";
 });
+
 No.addEventListener("click", () => {
-  const respondno = document.createElement("div");
-
-  container.appendChild(respondno);
-  response.textContent =
-    "no worries, I will be your Valentine, accept these flowers";
-  respondno.appendChild(response);
-
-  respondno.innerHTML = `
-    
-     
-   <img src="https://cdn.euroflorist.com/Products/600x700/DE21VALMM04S_1.jpg" alt="">
-  `;
+  const image = document.createElement("img");
+  image.src = "https://cdn.euroflorist.com/Products/600x700/DE21VALMM04S_1.jpg";
+  container.appendChild(response);
+  container.appendChild("image");
 });
